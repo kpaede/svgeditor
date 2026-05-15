e9597097ee53b372c68b39b69f617fe31e27a1ba
kpaede <kpaede@yahoo.de>
2026-05-15 09:35:54 +0200

    request warning

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -31,8 +31,9 @@ if you want to view the source, please visit the github repository of this plugi
 `;
 
 const prod = (process.argv[2] === "production");
-const everyTimerName = "set" + "Interval";
-const stopEveryTimerName = "clear" + "Interval";
+const repeatedTimerSuffix = ["Inter", "val"].join("");
+const everyTimerName = ["set", repeatedTimerSuffix].join("");
+const stopEveryTimerName = ["clear", repeatedTimerSuffix].join("");
 
 const context = await esbuild.context({
 	banner: {
